9fb42fe1a16a3d9acf58cd57b9f66721abff0e86
Rumyana Paneva <r.paneva@gmail.com>
2018-07-05 13:00:57 +0300

    Commit update modules

--- src/com/lol/Consts.java
+++ src/com/lol/Consts.java
@@ -1,7 +1,5 @@
 package com.lol;
 
-import java.util.ArrayList;
-
 public class Consts {
 
 
@@ -63,27 +61,5 @@ public class Consts {
     static final int PANTHEON_ABILITY_POWER = 10;
     static final boolean PANTHEON_IS_DEAD = false;
     static final String PANTHEON_TYPE = "Bruiser";
-
-
-    //  Items
-    static final int HUNTERS_MACHETE_ADD = 15;
-    static final int HUNTERS_MACHETE_PRICE = 50 ;
-    static final String HUNTERS_MACHETE_TYPE = "WEAPON";
-
-    static final int BILGWATER_CUTLASS_ADD = 18;
-    static final int BILGWATER_CUTLASS_PRICE = 90 ;
-    static final String BILGWATER_CUTLASS_TYPE = "WEAPON";
-
-    static final int AEGIS_OF_THE_LEGION_ADD = 13 ;
-    static final int AEGIS_OF_THE_LEGION_PRICE = 70;
-    static final String AEGIS_OF_THE_LEGION_TYPE = "SHIELD";
-
-    static final int DORANS_SHIELD_ADD = 10;
-    static final int DORANS_SHIELD_PRICE = 20;
-    static final String DORANS_SHIELD_TYPE = "SHIELD";
-
-    static final int ARCANE_SWEEPER_ADD = 7 ;
-    static final int ARCANE_SWEEPER_PRICE = 65;
-    static final String ARCANE_SWEEPER_TYPE = "TRINKET";
 
 }
--- src/com/lol/Item.java
+++ src/com/lol/Item.java
@@ -2,24 +2,24 @@ package com.lol;
 
 public class Item {
 
-    private Consts constants = new Consts();
+    private ItemConsts constants = new ItemConsts();
 
     public void buyItem(Hero hero, Items item) throws NoSuchFieldException, IllegalAccessException {
         String itemPrise = item + "_PRICE";
         String itemType = item + "_TYPE";
 
-        if (hero.getGold() >= Consts.class.getDeclaredField(itemPrise).getInt(constants)) {
+        if (hero.getGold() >= ItemConsts.class.getDeclaredField(itemPrise).getInt(constants)) {
 
             hero.getListHeroItems().add(item);
 
             for (Items oldItem : hero.getEquippedItems()) {
-                if(((String) Consts.class.getDeclaredField(oldItem + "_TYPE").get(constants)).equals(((String) Consts.class.getDeclaredField(itemType).get(constants)))){
+                if(( ItemConsts.class.getDeclaredField(oldItem + "_TYPE").get(constants)).equals(( ItemConsts.class.getDeclaredField(itemType).get(constants)))){
                     unEquipItem(hero, oldItem);
                 }
             }
 
             equipItem(hero, item);
-            hero.setGold(hero.getGold() - Consts.class.getDeclaredField(itemPrise).getInt(constants));
+            hero.setGold(hero.getGold() - ItemConsts.class.getDeclaredField(itemPrise).getInt(constants));
         }
     }
 
@@ -28,13 +28,14 @@ public class Item {
         String itemBenefit = item + "_ADD";
         String itemType = item + "_TYPE";
 
+
         if (hero.getListHeroItems().contains(item)) {
 
-            if (((String) Consts.class.getDeclaredField(itemType).get(constants)).equals("WEAPON")) {
-                hero.setAttackDamage(hero.getAttackDamage() + Consts.class.getDeclaredField(itemBenefit).getInt(constants));
+            if (( ItemConsts.class.getDeclaredField(itemType).get(constants)).equals("WEAPON")) {
+                hero.setAttackDamage(hero.getAttackDamage() + ItemConsts.class.getDeclaredField(itemBenefit).getInt(constants));
             }
-            if (((String) Consts.class.getDeclaredField(itemType).get(constants)).equals("SHIELD")) {
-                hero.setArmor(hero.getArmor() + Consts.class.getDeclaredField(itemBenefit).getInt(constants));
+            if (( ItemConsts.class.getDeclaredField(itemType).get(constants)).equals("SHIELD")) {
+                hero.setArmor(hero.getArmor() + ItemConsts.class.getDeclaredField(itemBenefit).getInt(constants));
             }
 
             hero.getEquippedItems().add(item);
@@ -47,11 +48,11 @@ public class Item {
 
         if(hero.getEquippedItems().contains(item)) {
 
-            if (((String) Consts.class.getDeclaredField(itemType).get(constants)).equals("WEAPON")) {
-                hero.setAttackDamage(hero.getAttackDamage() - Consts.class.getDeclaredField(itemBenefit).getInt(constants));
+            if (( ItemConsts.class.getDeclaredField(itemType).get(constants)).equals("WEAPON")) {
+                hero.setAttackDamage(hero.getAttackDamage() - ItemConsts.class.getDeclaredField(itemBenefit).getInt(constants));
             }
-            if (((String) Consts.class.getDeclaredField(itemType).get(constants)).equals("SHIELD")) {
-                hero.setArmor(hero.getArmor() - Consts.class.getDeclaredField(itemBenefit).getInt(constants));
+            if (( ItemConsts.class.getDeclaredField(itemType).get(constants)).equals("SHIELD")) {
+                hero.setArmor(hero.getArmor() - ItemConsts.class.getDeclaredField(itemBenefit).getInt(constants));
             }
 
             hero.getEquippedItems().remove(item);
