4f4e363ac7cdf8cddd11e4cca93385c1f9e5d419
Rumyana Paneva <r.paneva@gmail.com>
2018-07-12 20:36:38 +0300

    new Diagram

--- src/com/lol/Item.java
+++ src/com/lol/Item.java
@@ -6,8 +6,28 @@ public class Item {
 
     private ItemConsts constants = new ItemConsts();
 
-    public Item() {
+    public String toString() {
+        return getName()+
+                ", \taddDamage=" + getAddDamage() +
+                ", \taddArmor=" + getAddArmor() +
+                ", \taddMagic=" + getAddMagic() +
+                ", \tprice=" + getPrice() +
+                ';';
+    }
 
+    private Items name;
+    private int addDamage;
+    private int addArmor;
+    private int addMagic;
+    private int price;
+
+    public Item()  {
+
+    }
+
+    public Item(Items name) throws NoSuchFieldException, IllegalAccessException{
+        setName(name);
+        setStats(name);
     }
 
     public void buyItem(Hero hero, Items item) throws NoSuchFieldException, IllegalAccessException, InvalidStatsException {
@@ -18,7 +38,7 @@ public class Item {
             hero.getListHeroItems().add(item);
             equipItem(hero, item);
             if (hero.getEquippedItems().size() > ItemConsts.MAX_EQUIPPED_ITEMS) {
-                System.out.println("You have equipped too much items, which one you want to unequipped from next list:");
+                System.out.println("You have equipped too much items, which one you want to unequip from next list:");
                 System.out.println(hero.getEquippedItems());
                 oldItem = in.nextLine().toUpperCase();
                 System.out.println();
@@ -58,9 +78,9 @@ public class Item {
     public void listItem() throws NoSuchFieldException, IllegalAccessException {
         System.out.println("Available Items: ");
         System.out.println("-------------------------------------");
-        int itemNumber = 1;
+        int itemNumber=1;
         for (Items item : Items.values()) {
-            System.out.print(itemNumber + "\t" + item.toString() + "\t");
+            System.out.print(itemNumber+"\t"+item.toString() + "\t");
             if (item.toString().length() < 17) {
                 System.out.print("\t");
             }
@@ -72,5 +92,66 @@ public class Item {
             itemNumber++;
         }
     }
+
+    public Items stringToItems(String name){
+        for (Items items : Items.values()) {
+            if (name.equals(items.toString())) {
+                return items;
+            }
+        }
+        return null;
+    }
+
+    private void setStats(Items item) throws NoSuchFieldException, IllegalAccessException {
+        setAddDamage(ItemConsts.class.getDeclaredField(item + "_ADD_DAMAGE").getInt(constants));
+        setAddArmor(ItemConsts.class.getDeclaredField(item + "_ADD_ARMOR").getInt(constants));
+        setAddMagic(ItemConsts.class.getDeclaredField(item + "_ADD_MAGIC").getInt(constants));
+        setPrice(ItemConsts.class.getDeclaredField(item + "_PRICE").getInt(constants));
+    }
+
+    private int getAddDamage() {
+        return addDamage;
+    }
+
+    private void setAddDamage(int addDamage) {
+        this.addDamage = addDamage;
+    }
+
+    private int getAddArmor() {
+        return addArmor;
+    }
+
+    private void setAddArmor(int addArmor) {
+        this.addArmor = addArmor;
+    }
+
+    private int getAddMagic() {
+        return addMagic;
+    }
+
+    private void setAddMagic(int addMagic) {
+        this.addMagic = addMagic;
+    }
+
+    private int getPrice() {
+        return price;
+    }
+
+    private void setPrice(int price) {
+        this.price = price;
+    }
+
+    private Items getName() {
+        return name;
+    }
+
+    private void setName(Items name) {
+        this.name = name;
+    }
+
+//    System.out.println("If you want to view the characteristics of the item, type it's name: ");
+//    String name = in.nextLine();
+//    item = new Item(newItem.stringToItems(name));
+//    System.out.println(item);
 
 }
